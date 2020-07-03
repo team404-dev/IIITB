ec9c31ba211ea091228e4af58ba9ca000de1bf5e
AnantRaj <anantaditya6@gmail.com>
2020-07-03 12:30:29 +0530

    Recently updated by Anant Raj

--- app/src/main/java/com/example/iiitb_connects/SearchFragment.java
+++ app/src/main/java/com/example/iiitb_connects/SearchFragment.java
@@ -4,19 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SearchFragment extends Fragment {
-
-    @Nullable
+    Button logOut;
+    FirebaseAuth mAuth;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        SearchView searchView = view.findViewById(R.id.searchView);
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.frameLayout,new FragmentSearch2())
+                        .commit();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
 
+    //    logOut = view.findViewById(R.id.logOut);
+    /*    logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });  */
         return view;
     }
 }
