0cc123b0ecda52fefdc4c85b466faa0e868fe611
AnantRaj <anantaditya6@gmail.com>
2020-07-16 22:02:53 +0530

    Search Question Update on 16/7/2020

--- app/src/main/java/com/example/iiitb_connects/AddQuestionActivity.java
+++ app/src/main/java/com/example/iiitb_connects/AddQuestionActivity.java
@@ -1,5 +1,6 @@
 package com.example.iiitb_connects;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,8 +14,11 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddQuestionActivity extends AppCompatActivity {
     //Views
@@ -29,6 +33,7 @@ public class AddQuestionActivity extends AppCompatActivity {
 
     //firebase
     DatabaseReference mRef;
+    DatabaseReference mRefAns;
     FirebaseAuth mAuth;
 
     @Override
@@ -50,7 +55,7 @@ public class AddQuestionActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         mRef = FirebaseDatabase.getInstance().getReference("Questions Asked").child(mAuth.getCurrentUser().getUid());
-
+    //    mRefAns = FirebaseDatabase.getInstance().getReference("Answers");
         closebutton();
         submitYourQuestion();
 
@@ -69,7 +74,7 @@ public class AddQuestionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 question = askAQuestionEditText.getText().toString().trim();
-                if (question.equals("")){
+                if (question.equals("") || question.equals(null)){
                     Toast toast = Toast.makeText(AddQuestionActivity.this, "Type a Question!", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,0);
                     toast.show();
@@ -81,8 +86,9 @@ public class AddQuestionActivity extends AppCompatActivity {
                 if (!last.equals("?") && !last.equals("!") && !last.equals(".")){
                     question = question + "?";
                 }
-                QuestionInfo ques = new QuestionInfo(question,0);
                 DatabaseReference mDRef = mRef.push();
+                QuestionInfo ques = new QuestionInfo(question,0,mDRef.getKey());
+        //        mRefAns.child(mDRef.getKey()).child("Number of Answers").setValue("0");
                 mDRef.setValue(ques);
                 progressBar.setVisibility(View.GONE);
                 waitTextView.setVisibility(View.GONE);
